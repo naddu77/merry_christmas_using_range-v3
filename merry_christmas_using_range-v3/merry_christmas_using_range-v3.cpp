5ac3feb3fd6961dd91b0aa0c2f11f919a88b1635
Hyung-Suk Lho <nonaddu@gmail.com>
2020-12-19 17:09:50 +0900

    * Done!

--- merry_christmas_using_range-v3/merry_christmas_using_range-v3.cpp
+++ merry_christmas_using_range-v3/merry_christmas_using_range-v3.cpp
@@ -1,78 +1,101 @@
-﻿// merry_christmas_using_range-v3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
+#include <range/v3/all.hpp>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <iterator>
 #include <algorithm>
-#include <range/v3/all.hpp>
+#include <concepts>
 
-int main()
+namespace rv = ranges::views;
+using namespace std::string_literals;
+
+template <std::integral T, std::convertible_to<std::string>... Ts>
+auto MakeChristmasTree(T const level, Ts const&... messages)
 {
-	auto make_christmas_tree = [](auto const level, auto const& message) {
-		auto index = ranges::view::iota(3)
-			| ranges::view::filter([](auto const e) { return e % 2 == 1; })
-			| ranges::view::take(level)
-			;
-		auto rindex = index
-			| ranges::view::reverse
-			;
-
-		auto zipped = ranges::view::zip(index, rindex);
-
-		std::vector<std::vector<std::string>> christmas_tree = zipped
-			| ranges::view::transform([](auto const& n) {
-				auto make_triangle = [](auto const n) {
-					auto spaces = ranges::view::iota(1)
-						| ranges::view::transform([](auto const e) {
-							return std::string(e - 1, ' ');
-						})
-						| ranges::view::take(n)
-						| ranges::view::reverse
-						;
-
-					auto stars = ranges::view::iota(1)
-						| ranges::view::transform([](auto const e) {
-							auto stars = std::string(e, '*');
-							std::string stars_with_space = stars | ranges::view::intersperse(' ');
-
-							return stars_with_space;
-						})
-						| ranges::view::take(n)
-						;
-
-					std::vector<std::string> result = ranges::view::zip_with([](auto const& s1, auto const& s2) { return s1 + s2; }, spaces, stars);
-
-					return result;
-				};
-
-				auto indent = [](auto const n) {
-					return ranges::make_pipeable([=](auto&& rng) {
-						auto input = ranges::view::repeat(n)
-							| ranges::view::transform([](auto const n) { return std::string(n, ' '); })
-							| ranges::view::take(ranges::distance(rng))
-							;
-
-						std::vector<std::string> result = ranges::view::zip_with([](auto const& s1, auto const& s2) { return s1 + s2; }, input, rng);
-
-						return result;
-					});
-				};
-
-				return make_triangle(n.first) | indent(n.second);
+	auto const msg_max_len{
+		1 + ranges::accumulate(
+			std::vector<std::string>{ messages... },
+			int{},
+			[](auto const acc, auto const& msg) {
+				return std::max(acc, static_cast<int>(ranges::size(msg)));
 			})
-			;
-		std::vector<std::string> result = christmas_tree 
-			| ranges::view::join
-			;
-
-		return result |= ranges::action::push_back("")
-			| ranges::action::push_back(std::string(ranges::max(1, (static_cast<int>(std::size(result.back())) - static_cast<int>(std::size(message))) / 2 + 2), ' ') + message);;
+	};
+	auto const center_pos{ 1 + level * 4 };
+	auto index{
+		rv::iota(3)
+		| rv::stride(2)
+		| rv::take(level)
+	};
+	auto rindex{
+		index
+		| rv::reverse
 	};
 
-	ranges::copy(make_christmas_tree(3, "Merry Christmas!!!!"), ranges::ostream_iterator<std::string>(std::cout, "\n"));
+	return rv::zip(index, rindex)
+		| rv::transform([msg_max_len, center_pos](auto const& p) {
+			auto make_triangle{ [](auto const n) {
+				auto spaces{ rv::iota(1)
+					| rv::transform([](auto const e) {
+						return rv::repeat_n(' ', e);
+					})
+					| rv::take(n)
+					| rv::reverse
+				};
+				auto stars{ rv::iota(1)
+					| rv::transform([](auto const e) {
+						return rv::repeat_n('*', e) | rv::intersperse(' ');
+					})
+					| rv::take(n)
+				};
+
+				return rv::zip_with([](auto const& s1, auto const& s2) { return rv::concat(s1, s2); }, spaces, stars);
+			} };
+			auto indent{ [msg_max_len, center_pos](auto const n) {
+				return ranges::make_pipeable([=](auto&& rng) {
+					auto const padding{ std::max<int>(0, (msg_max_len - center_pos) / 2) };
+					auto input{ rv::repeat(n)
+						| rv::transform([msg_max_len, padding](auto const n) { return rv::repeat_n(' ', n - 3 + padding); })
+						| rv::take(ranges::distance(rng))
+					};
+
+					return rv::zip_with([](auto const& s1, auto const& s2) { return rv::concat(s1, s2); }, input, std::move(rng));
+				});
+			} };
+
+			return make_triangle(p.first) | indent(p.second);
+		})
+		| rv::join
+		| ranges::to<std::vector<std::string>>()
+		| ranges::actions::push_back(std::vector<std::string>(3, std::string(std::max(msg_max_len / 2 - 1, center_pos / 2), ' ') + "***"))
+		| ranges::actions::push_back({
+			""s,
+			std::string((std::max(msg_max_len, center_pos) - ranges::size(messages)) / 2 + 2, ' ') + messages...
+		});
+}
+int main()
+{
+	auto n{ 0 };
+
+	do {
+		std::cout << "How big would you like to make a Christmas tree? [n >= 3]\n> ";
+		
+		if (std::string line; std::getline(std::cin, line))
+		{
+			try
+			{
+				n = std::stoi(line);
+			}
+			catch (...)
+			{
+				std::cerr << "Please enter a number of 3 or more.\n";
+				
+				n = 0;
+			}
+		}
+	} while (n < 3);
+
+	ranges::copy(MakeChristmasTree(n, "Merry Christmas!!!!", "C++Korea"), ranges::ostream_iterator(std::cout, "\n"));
 
 	return 0;
 }
